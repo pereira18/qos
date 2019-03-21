b95a41350fce19faf7c899bdfb285fd4e5efb31e
rodrigo <rodrigo58@live.com.pt>
2019-03-21 10:57:00 +0000

    layout

---
+++ app/src/main/java/com/example/metrics/Activity2.java
@@ -0,0 +1,22 @@
+package com.example.metrics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity2 extends AppCompatActivity {
+    Button backButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/metrics/MainActivity.java
+++ app/src/main/java/com/example/metrics/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.metrics;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button1;
+    private Button button2;
+    private Button button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openExperience(1);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openExperience(2);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openExperience(3);
+            }
+        });
+    }
+    public void openExperience(int ExpId){
+        Intent intent = new Intent();
+        switch (ExpId) {
+            case 1:
+                intent = new Intent(this, Activity1.class);
+                break;
+            case 2:
+                intent = new Intent(this, Activity2.class);
+                break;
+            case 3:
+                intent = new Intent(this, Activity3.class);
+                break;
+            default:
+                break;
+        }
+        startActivity(intent);
     }
 }
